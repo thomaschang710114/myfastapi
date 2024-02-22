02be9431cfdcedd3e4b69fb4cf537ea0ac770238
Chang Thomas <thomaschang@ChangdeMacBook-Pro.local>
2024-02-22 14:42:28 +0800

    modify require

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# LINE Bot 設定
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_USER_ID = os.environ.get("LINE_USER_ID")
